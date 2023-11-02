12ab3997419ee2ac30ae1f90f4d37794864ff98c
attaryu <mattarannaufal@gmail.com>
2023-11-03 05:20:44 +0700

    add: link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,48 @@
+import { Link as LinkReactRouter, useLocation } from 'react-router-dom';
+
+type Props = {
+  to: string,
+  type?: 'normal' | 'navbar' | 'button primary' | 'button secondary',
+  children: string,
+  size?: 'sm' | 'normal',
+};
+
+export default function Link({ to, type = 'normal', size = 'normal', children }: Props) {  
+  if (type === 'navbar') {
+    const location = useLocation().pathname;
+    const current = location == to && 'before:scale-x-100';
+
+    return (
+      <LinkReactRouter
+        to={to}
+        className={`relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:origin-right before:w-full before:h-0.5 before:bg-red-500 before:scale-x-0 before:transition-transform hover:before:origin-left hover:before:scale-x-100 before:duration-300 before:ease-in-out before:-z-10 -z-0 ${current}`}
+      >
+        {children}
+      </LinkReactRouter>
+    );
+  } else if (type.includes('button')) {
+    const isPrimary = type.includes('primary');
+    const bg = isPrimary ? 'bg-zinc-900' : 'bg-white';
+    const text = isPrimary ? 'text-white' : 'text-zinc-900';
+    const linkSize = size == 'sm' ? 'px-3.5 py-1.5' : 'text-xl px-4 py-2'
+    
+    return (
+      <LinkReactRouter
+        to={to}
+        className={`font-bold rounded-md ${bg} ${text} ${linkSize}`}
+      >
+        {children}
+      </LinkReactRouter>
+    );
+  }
+
+  return (
+    <LinkReactRouter
+      to={to}
+      className="text-blue-500 font-medium underline underline-offset-3"
+    >
+      {children}
+    </LinkReactRouter>
+  );
+}
+  
