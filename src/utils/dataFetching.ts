d521f28fe9afd669981584768e021fb124860323
attaryu <mattarannaufal@gmail.com>
2023-11-05 00:27:58 +0700

    modify: customable filter

--- src/utils/dataFetching.ts
+++ src/utils/dataFetching.ts
@@ -1,12 +1,12 @@
-import { category, product } from '@/data/index';
+import { Product, category, product } from '@/data/index';
 
 export const getAllCategory = () => category;
 
-export function getProducts(length: number = product.length, categoryId: number | null = null) {
-  let copyProduct = [...product];
-
-  if (categoryId)
-    copyProduct = product.filter((item) => item.category.includes(categoryId));
+export function getProducts(
+  length: number = product.length,
+  filter?: (item: Product) => boolean
+) {
+  const copyProduct = filter ? product.filter(filter) : [...product];
 
   for (let i = copyProduct.length - 1; i >= 0; i--) {
     const randomIndex = Math.floor(Math.random() * copyProduct.length);
