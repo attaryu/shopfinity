b3e9a8622f28127a57f01e84cb22a8a9184af950
attaryu <mattarannaufal@gmail.com>
2023-11-05 00:28:37 +0700

    add: data store

---
+++ src/stores/index.tsx
@@ -0,0 +1,15 @@
+import { signal } from '@preact/signals'; 
+
+import type { Signal } from '@preact/signals';
+
+type DataStore = {
+  data: {
+    email?: string,
+  },
+  cart: number[],
+}
+
+const store: Signal<DataStore> = signal({
+  data: {},
+  cart: [],
+});
