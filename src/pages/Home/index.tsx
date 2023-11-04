54bce12201a329a763335a1790e2bd71efade96d
attaryu <mattarannaufal@gmail.com>
2023-11-05 00:27:36 +0700

    modify: carousel tambahan

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Carousel from './Carousel';
 import CategoryTag from './CategoryTag';
 import Section from './Section';
@@ -11,7 +12,7 @@ export default function Home() {
   return (
     <>
       <header>
-        <Carousel dots arrows>
+        <Carousel dots>
           {ImageSliderLinks.map((url) => (
             <div key={url} className="px-1 pb-2 h-[28rem] w-full">
               <img src={url} alt="" className="h-full w-full object-cover rounded-lg" />
@@ -24,10 +25,43 @@ export default function Home() {
         </div>
       </header>
 
-      <main className="mt-28 flex flex-col gap-28">
+      <main className="mt-28 flex flex-col gap-32">
         <Section title="Hot Sale" data={getProducts(10)} />
-        <Section title="Sepatu Terlaris" data={getProducts(10, 4)} />
-        <Section title="Tas Terbaru" data={getProducts(10, 5)} />
+
+        <section>
+          <h1 class="text-center text-4xl mb-14 font-bold">Fashion Pilihan</h1>
+
+          <Carousel dots>
+            {getProducts(5).map((item) => (
+              <div className="w-full">
+                <div className="flex h-80 w-2/3 m-auto p-10 rounded-md bg-zinc-200 gap-10">
+                  <div className="w-full flex flex-col">
+                    <h2 className="text-3xl font-bold">{item.name}</h2>
+
+                    <p className="mt-5 text-xl font-semibold text-zinc-800">Rp {item.price.toLocaleString('id')}</p>
+
+                    <div className="mt-auto flex gap-3 h-9">
+                      <Link
+                        to={`/product/${item.id}`}
+                        className="bg-zinc-950 text-white grid place-items-center w-1/2 rounded-md "
+                      >
+                        Lebih Lengkap
+                      </Link>
+                      <button className="h-full aspect-square border-[1px] border-zinc-950 grid place-items-center rounded-md">
+                        <i className="fi fi-rs-shopping-cart-add grid place-items-center" />
+                      </button>
+                    </div>
+                  </div>
+
+                  <img src={item.image} alt={item.name} className="w-1/2 h-full object-cover rounded-lg" />
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </section>
+
+        <Section title="Sepatu Terlaris" data={getProducts(10, (item) => item.category.includes(4))} />
+        <Section title="Tas Terbaru" data={getProducts(10, (item) => item.category.includes(5))} />
       </main>
     </>
   );
