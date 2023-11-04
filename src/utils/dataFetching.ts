e909d2f2834cf1ec94395486f235bb4cdc6fcebc
attaryu <mattarannaufal@gmail.com>
2023-11-04 11:08:02 +0700

    add: fungsi getProducts

--- src/utils/dataFetching.ts
+++ src/utils/dataFetching.ts
@@ -1,3 +1,18 @@
-import { category, product } from '../data';
+import { category, product } from '@/data/index';
 
 export const getAllCategory = () => category;
+
+export function getProducts(length: number = product.length, categoryId: number | null = null) {
+  let copyProduct = [...product];
+
+  if (categoryId)
+    copyProduct = product.filter((item) => item.category.includes(categoryId));
+
+  for (let i = copyProduct.length - 1; i >= 0; i--) {
+    const randomIndex = Math.floor(Math.random() * copyProduct.length);
+
+    [copyProduct[randomIndex], copyProduct[i]] = [copyProduct[i], copyProduct[randomIndex]];
+  }
+
+  return copyProduct.slice(0, length);
+}
