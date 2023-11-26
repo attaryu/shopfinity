63256455c6a9f0dd8429b3a730f018b75626374e
attaryu <mattarannaufal@gmail.com>
2023-11-26 11:10:18 +0700

    fixed accessibility

--- src/pages/MyCart/Card.tsx
+++ src/pages/MyCart/Card.tsx
@@ -19,12 +19,14 @@ export default function Card({ id, quantity }: Readonly<Omit<Cart, 'price'>>) {
   } = useStore();
   const navigate = useNavigate();
 
+  const detailProductHandler = () => navigate(`/product/${id}`);
+
   return (
     <li className="w-full flex p-3 bg-zinc-100 rounded-lg">
-      <img src={image} alt={name} className="w-24 overflow-clip aspect-square rounded-lg relative before:content-['Image_not_found'] before:absolute before:w-full before:h-full before:bg-zinc-200 before:top-0 before:left-0 before:grid before:place-items-center before:text-zinc-500 before:text-xs before:p-2.5 before:text-center" />
+      <img src={image} alt={name} className="w-24 overflow-clip aspect-square rounded-lg relative before:content-['Image_not_found'] before:absolute before:w-full before:h-full before:bg-zinc-200 before:top-0 before:left-0 before:grid before:place-items-center before:text-zinc-500 before:text-xs before:p-2.5" />
 
       <div className="ml-5 flex flex-col justify-center gap-3 w-full">
-        <h2 className="font-semibold text-xl overflow-hidden leading-6 h-6 w-full before:w-1/5 before:h-6 before:bg-gradient-to-l before:from-zinc-100 before:to-transparent relative before:bottom-0 before:right-0 before:absolute cursor-pointer" onClick={() => navigate(`/product/${id}`)}>{name}</h2>
+        <h2 className="font-semibold text-xl overflow-hidden leading-6 h-6 w-full before:w-1/5 before:h-6 before:bg-gradient-to-l before:from-zinc-100 before:to-transparent relative before:bottom-0 before:right-0 before:absolute cursor-pointer" tabIndex={0} onClick={detailProductHandler} onKeyPress={detailProductHandler}>{name}</h2>
 
         <div className="flex gap-4 items-center">
           {isDiscount && <p className="py-0.5 px-1.5 border-2 font-semibold text-red-500 border-red-500 rounded-md">Rp. {discountPrice!.toLocaleString('id')}</p>}
